53d16b051bef7593aa548eda2802f2481717faa5
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-05-18 09:04:44 +0900

    paiza d

--- rensyu.rb
+++ rensyu.rb
@@ -1,13 +1,3 @@
-#改行区切りで正の整数がn個入力されるので、n個の整数のうち、5以上のものを全て足し合わせた値を出力してください。
-loop = gets.chomp.to_i
-ans = 0
-
-(1..loop).each do
-    n = gets.chomp.to_i
-    if n >= 5
-        ans += n
-    end    
-end
-
-puts ans 
-    
+#複数の値を取得し、改行して出力
+str = gets
+puts str.split
