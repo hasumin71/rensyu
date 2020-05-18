b7fb0b9feed8bbd0f6d7ef2dc9bfdb689ccd9475
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-05-18 12:43:35 +0900

    paiza c

--- rensyu.rb
+++ rensyu.rb
@@ -1,3 +1,13 @@
-#複数の値を取得し、足して出力
-nums = gets.split(' ')
-puts nums[0].to_i + nums[1].to_i
+count = gets.chomp.to_i #行数を指定する値を取得
+sum = 0 #合計値を意味する変数の初期値設定
+
+(1..count).each do #一から取得した値まで繰り返す。
+    line = gets.chomp.split(" ") #二つの整数を一定の規則で区切って配列にする処理
+    if line[0].to_i == line[1].to_i #もしインデックス0と1が同じ値だったら . . .
+        sum += line[0].to_i * line[1].to_i #sumに,ind[1]と[2]をかけてsumに足す
+    else    #そうじゃなかったら
+        sum += line[0].to_i + line[1].to_i  #1と２を足してsumに足す
+    end
+end
+
+puts sums
