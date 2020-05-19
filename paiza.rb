60e878caeef9761aefafacf6ac24f98d0fd8af74
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-05-19 15:35:23 +0900

    文字と整数の組みのソート

--- paiza.rb
+++ paiza.rb
@@ -35,10 +35,26 @@ end
 #昇順ソート出力
 gets.chomp.to_i # 行数の入力を受け取るが使用しないため変数に代入しない
 
-array = gets.chomp.split(' ') #スペース区切りで値を取得
+array = gets.chomp.split(' ') #スペース区切りで値を取得 #補足: sliceは文字列データ splitは配列データ
 
-(0..(array.size - 1)).each do |i| #配列[0]からarray配列の数を、変数iに渡して回す
+(0..(array.size - 1)).each do |i| #配列[0]からarray配列の数を、変数iに渡して回す(配列は0から数えるためarrayの数から1引く)
   array[i] = array[i].to_i #arrayの順に回ってきた値を、整数に変えてarray[i]に渡す
 end
 
 puts array.sort #arrayを昇順で渡す。
+
+#文字と整数の組のソート
+num = gets.chomp.to_i #整数を受け取る
+array = {} #ハッシュをarrayに代入,次に受け取る複数の要素をキーとバリューの関係で保管したいから。
+
+(1..num).each do #一から受け取った数をeachで回す。
+  line = gets.chomp.split(' ') #取得した値を半角スペース区切りで配列に入れる,変数に代入
+  array[line[1].to_i] = line[0] #変数line[0]を[line[1].to_i]に代入
+end
+
+array = array.sort #ローマ字が持つ整数の値で昇順に並び替える
+
+array.each do |ele| #配列を回す
+  puts ele[1] #ローマ字のみ出力
+end
+
