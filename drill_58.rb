3db9c891deeb7e339d6305e77bcc9fb4e7a5dba4
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-05-20 12:10:56 +0900

    daily_drill 58

---
+++ drill_58.rb
@@ -0,0 +1,12 @@
+def sleep_in(weekday,vacation)
+  if (weekday != true) || (vacation == true)
+    puts true
+  else 
+    puts false
+  end
+end
+
+weekday = true 
+vacation = false 
+
+sleep_in(weekday,vacation)
