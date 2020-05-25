fabcd4a2a204b1f0a9e097d5203c2671ff7cc963
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-05-25 09:43:49 +0900

    how to use a JOIN

--- git_memo.rb
+++ git_memo.rb
@@ -7,4 +7,12 @@ WHERE height > ( #取得した⓶よりも高いheightを持った選手名と�
   FROM players
 )
 ;
-
+#joinの使い方 
+SELECT * #全ての項目を検索する #⓶結合された後にセレクトが実行される。
+#joinの実行順序 #①まずは最初にテーブルを結合する
+FROM players 
+JOIN countries #fromで指定したテーブル以外の紐付けしたいテーブルを指定。
+ON players.country_id = countries.id #onは結合条件,「on テーブル名.外部キー = テーブル名.主キー」
+WHERE countries.name = "日本" #whereでもテーブル名を指定することができる。
+AND height >= 180 #andはwhereに複数の条件を指定することができる。
+;
