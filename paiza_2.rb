254ce8f01edb3f11b97022664af6ba570f5f1f23
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-05-29 10:27:29 +0900

    paiza_blood

--- paiza_2.rb
+++ paiza_2.rb
@@ -11,3 +11,16 @@ end
 user2blood.each do |key, value|
   puts "#{key} #{value}"
 end
+
+#同様のキーの血液型を出力
+user = gets.chomp #キーとなる値を取得
+num = gets.chomp.to_i #入力される列を取得
+
+user2blood = {} #からのハッシュを用意
+
+(1..num).each do #
+  array = gets.chomp.split(' ') #名前と血液型のセットを取得
+  user2blood[array[0]] = array[1] #先にからのハッシュを用意した後で、変数[⓵取得した文字列の変数[0]] = ⓵[1] これでキーとバリューの関係になる
+end
+
+puts user + ' ' + user2blood[user] #ユーザー名と半角スペースと
