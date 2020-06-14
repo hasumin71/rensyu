64cfa5a40aa00ac1f367a45358296ef4250faca7
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-06-14 21:32:09 +0900

    fibonacci_another-ans still working

--- try.rb
+++ try.rb
@@ -1,7 +1,27 @@
-line = gets.chomp.to_i
-space = " "
-a = "a"
+n,m = gets.chomp.split(" ").map(&:to_i)
 
-line.times do |i|
-  puts space*(line-i)+a*(i+1)
-end   #SrtingクラスとIntegerクラスの四則演算子で挙動が異なる
+decolights = gets.chomp.split(" ").map(&:to_i)
+
+ins = gets.chomp.to_i 
+
+#平均を出すためのaverageメソッド
+
+
+ins.times do  #調査の回数
+    st,en = gets.chomp.split(" ").map(&:to_i)
+    range = (en - st) + 1
+    result = []
+    range.times do |i|
+        result << decolights[(i+st)-1]
+    end
+    if result.average < m 
+       result.length.times do |i| #timesの場合最初は必ず０が来る#次は1来るから範囲分足してくれる。
+           decolights[(i+st)-1] += (m - result.average)
+       end 
+    end    
+
+end
+
+#print最終行に改行がつかないやつ
+puts decolights.join(" ")
+    
