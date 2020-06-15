d8aa307591459cd73e0b47a38b782879ebe89770
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-06-15 11:48:35 +0900

    how to output fibo (another ans)

--- try.rb
+++ try.rb
@@ -1,27 +1,5 @@
-n,m = gets.chomp.split(" ").map(&:to_i)
-
-decolights = gets.chomp.split(" ").map(&:to_i)
-
-ins = gets.chomp.to_i 
-
-#平均を出すためのaverageメソッド
-
-
-ins.times do  #調査の回数
-    st,en = gets.chomp.split(" ").map(&:to_i)
-    range = (en - st) + 1
-    result = []
-    range.times do |i|
-        result << decolights[(i+st)-1]
-    end
-    if result.average < m 
-       result.length.times do |i| #timesの場合最初は必ず０が来る#次は1来るから範囲分足してくれる。
-           decolights[(i+st)-1] += (m - result.average)
-       end 
-    end    
-
-end
-
-#print最終行に改行がつかないやつ
-puts decolights.join(" ")
-    
+#配列でフィボナッチ数列を出力する方法
+def fib(n)
+    n.times.each_with_object([0,1]) { |num, obj| obj << obj[-2] + obj[-1] }
+  end
+puts fib(10).join(" ")
