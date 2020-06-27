22cf73af41483ec0d601a6c438d0e12cf7e28463
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-06-27 21:20:50 +0900

    Update boy&girl.rb

--- boy&girl.rb
+++ boy&girl.rb
@@ -1,8 +1,8 @@
 #男性20人、女性10人が到着した場合、どこで区切っても二つのグループのいずれも
 #男女の数が異なってしまうような到着順が何通りあるかを求めてください。
 boy, girl = 20, 10 
-boy, girl = boy + 1, girl + 1 
-ary = Array.new(boy * girl){0}
+boy, girl = boy + 1, girl + 1  #なぜ一足す？
+ary = Array.new(boy * girl){0} #210の０が入った配列ができる{}は要素
 ary[0] = 1  
 girl.times{|g|
  boy.times{|b|
