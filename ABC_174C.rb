0ec034abdc962fe2af6a75e09f428e557bb49687
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-08-22 16:26:39 +0900

    ABC_174C 修正

--- ABC_174C.rb
+++ ABC_174C.rb
@@ -1,19 +1,11 @@
-#modは余を取得する演算
-k = gets.chomp.to_i #kは倍数
-i = 1 #初期値
-a = 7 % k #7をkで割った余りがa
-h = a 
-if k != 7 #kが７ではない場合
-  until a == 0 #aが０になるまで繰り返す
-    a = (7 + a * 10) % k #
-    if a == h || i == k #なぜi＝＝k？
-      puts "-1" #倍数がない時は-1を返す
-      exit 
-    end 
-    i += 1 
-  end 
-else 
-  puts "1" #この1は何？
-  exit
+k = gets.to_i 
+a = 7
+ans = 0
+k.times do |i|
+  if a % k == 0
+    ans = (i+1)
+    break
+  end
+  a = (a * 10 + 7) % k
 end
-puts i 
+puts ans == 0 ? "-1" : ans
